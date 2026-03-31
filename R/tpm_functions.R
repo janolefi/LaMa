5b9e6aa97236999b152f2428447e68b5d1162fb0
Jan-Ole Koslik <101901166+janoleko@users.noreply.github.com>
2026-03-31 12:29:13 +0200

    updating examples in tpm() and tpm_g()

--- R/tpm_functions.R
+++ R/tpm_functions.R
@@ -47,13 +47,24 @@
 #' @import RTMB
 #'
 #' @examples
-#' # 2 states: 2 free off-diagonal elements
-#' par1 = rep(-1, 2)
-#' Gamma1 = tpm(par1)
+#' ## homogeneous Markov chain
+#' # 2 states: 2 = 2*(2-1) free off-diagonal elements
+#' par <- rep(-2, 2)
+#' Gamma <- tpm(par)
+#' # 3 states: 6 = 3*(3-1) free off-diagonal elements
+#' par <- rep(-3, 6)
+#' Gamma <- tpm(par)
+#' # 4 states: 12 = 4*(4-1) free off-diagonal elements
+#' par <- rep(-4, 12)
+#' Gamma <- tpm(par)
 #' 
-#' # 3 states: 6 free off-diagonal elements
-#' par2 = rep(-2, 6)
-#' Gamma2 = tpm(par2)
+#' ## inhomogeneous Markov chain
+#' # t.p.m. depends on covariates
+#' z1 <- runif(100); z2 <- runif(100) # 2 covariates
+#' Z <- cbind(1, z1, z2) # design matrix
+#' beta0 <- c(-2, -2); beta1 = c(1, -2); beta2 = c(2, -1) # coefficients for intercept and covariates
+#' beta <- cbind(beta0, beta1, beta2) # coefficient matrix; with intercepts!
+#' Gamma <- tpm(beta, Z) # array with 100 slices
 tpm <- function(
     beta, 
     Z = NULL, 
@@ -183,9 +194,13 @@ tpm <- function(
 #' @importFrom stats na.omit
 #'
 #' @examples
-#' Z = matrix(runif(200), ncol = 2)
-#' beta = matrix(c(-1, 1, 2, -2, 1, -2), nrow = 2, byrow = TRUE)
-#' Gamma = tpm_g(Z, beta)
+#' ## inhomogeneous Markov chain
+#' # t.p.m. depends on covariates
+#' z1 <- runif(100); z2 <- runif(100) # 2 covariates
+#' Z <- cbind(1, z1, z2) # design matrix
+#' beta0 <- c(-2, -2); beta1 = c(1, -2); beta2 = c(2, -1) # coefficients for intercept and covariates
+#' beta <- cbind(beta0, beta1, beta2) # coefficient matrix; with intercepts!
+#' #' Gamma <- tpm(beta, Z) # array with 100 slices
 tpm_g = function(Z, beta, 
                  Eta = NULL, 
                  byrow = FALSE, 
